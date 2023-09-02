f87f212f1a1fd0387b09138dc675fb261db396e9
Adriano Luzi <adriano.luz16i@gmail.com>
2023-09-02 00:21:04 -0300

    adding features

--- Js/main.js
+++ Js/main.js
@@ -2,7 +2,7 @@
  
 let hero = prompt( "Ingrese su Nombre de heroe:");
 let vidaHero = 100;
-let dañoBasicoHero = Math.random()*(10-1)+1;
+let dañoBasicoHero = Math.ceil(Math.random()*(10-1)+1);
 let armHero = 10;
 
 let encRandom ;
@@ -12,69 +12,70 @@ let vidaMons;
 let dañoMons;
 let armMons;
 
-let pasosHero = prompt( "Ingrese cuanto quiere caminar en el bosque" );
 
+function encuentro1() {
 
-for (let i = 0; i < pasosHero; i++) {
+    console.log("Un Goblin acaba de salir detras de un arbol!!");
 
-    encRandom = Math.ceil ( Math.random()*100); 
-
-    if (encRandom<=25){
-
-        console.log("Un Goblin acaba de salir detras de un arbol!!");
-
-        vidaMonster = Math.ceil(Math.random()*(20 - 10) + 10);
-
-        dañoMons = Math.ceil(Math.random()*(10-1)+1);
-
-        armMons = Math.ceil(Math.random()* 5) ;
-
-        accion=prompt("quiere ATACAR o HUIR?");
-
+            vidaMons = Math.ceil(Math.random()*(20 - 10) + 10);
+            dañoMons = Math.ceil(Math.random()*(10-1)+1);
+            armMons = Math.ceil(Math.random()* 5) ;
+            accion=prompt("quiere ATACAR o HUIR?");
         
 
-        if (accion==="ATACAR"){
-
-            while (vidaMons > 0 && vidaHero > 0){
-
-                if (dañoBasicoHero >= armMons){
-
-                    vidaMons-=dañoBasicoHero;
-                    if (vidaMons < 0){
-                        vidaMons = 0
+            if (accion==="ATACAR"){
+                while (vidaMons > 0 && vidaHero > 0){
+                    if (dañoBasicoHero >= armMons){
+                        vidaMons-=dañoBasicoHero;
+                        if (vidaMons < 0){
+                            vidaMons = 0
+                        }
+                        console.log(hero + " le cortó el brazo al goblin haciendole " + dañoBasicoHero + " de daño");
+                        if (vidaMons == 0 ) {
+                            console.log( "el goblin murio");
+                        }
                     }
 
-                    console.log(hero + " le cortó el brazo al goblin haciendole" + dañoBasicoHero + "de daño");
-                    
-                    if (vidaMons == 0 ) {
+                    if (dañoMons >= armHero){
 
-                        console.log( "el goblin murio");
+                        vidaHero-=dañoMons;
+
+                        console.log("El goblin apuñaló a " + hero + " con una daga haciendole " + dañoMons + " de daño" );
                     }
 
-                }
-
-                if (dañoMons >= armHero){
-
-                    vidaHero-=dañoMons;
-
-                    console.log("El goblin apuñaló a" + hero + "con una daga haciendole" + dañoMons + "de daño" );
-                }
-
-            } 
+                } 
             
 
 
-        }else if (accion=="HUIR"){
+            }else if (accion=="HUIR"){
 
-        }
+                console.log(hero + " ha huido exitosamente, el goblin grita desde la distancia");
 
-    }     
+            }
+
+}
+
+let pasosHero = prompt( "Ingrese cuanto quiere caminar en el bosque" );
+
+for (let i = 0; i < pasosHero; i++) {
+
+    encRandom = Math.ceil ( Math.random()*4);
     
-    if (  encRandom>25 && encRandom<=50){
 
-        console.log( hero + "encontró un tesoro!!!");
+    switch (encRandom) {
+        case 1:
+            encuentro1();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+
     }
-
+   
+   
 }
 
 
